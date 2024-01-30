612710a87792aca399ff586afe05da9dbdfb0c0d
Timofey72 <tima.rus-2007@mail.ru>
2024-01-30 15:43:46 +0500

    small seo; add robots.txt

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,10 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('exam.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
 
 handler404 = 'exam.views.handler404'
