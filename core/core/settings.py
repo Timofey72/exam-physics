32778413bac2fd55e37449755d80dafb244c7037
Timofey72 <tima.rus-2007@mail.ru>
2024-02-03 22:17:37 +0500

    change titles and allowed hosts

--- core/core/settings.py
+++ core/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '37.140.241.26', 'easy-ege.ru', 'www.easy-ege.ru']
 
 # Application definition
 
