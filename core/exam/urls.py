27273e82181228ce25bc3e619dd5faea61f88334
Timofey72 <tima.rus-2007@mail.ru>
2024-02-04 15:10:15 +0500

    add slash to path

--- core/exam/urls.py
+++ core/exam/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('variants', views.variants, name='variants'),
-    path('variants/<int:variant_id>', views.single_variant, name='single-variant'),
-    path('tasks', views.tasks, name='tasks'),
-    path('tasks/<int:task_id>', views.single_task, name='single-task'),
-    path('formulas', views.formulas, name='formulas'),
-    path('docs', views.docs, name='docs'),
+    path('variants/', views.variants, name='variants'),
+    path('variants/<int:variant_id>/', views.single_variant, name='single-variant'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('tasks/<int:task_id>/', views.single_task, name='single-task'),
+    path('formulas/', views.formulas, name='formulas'),
+    path('docs/', views.docs, name='docs'),
 ]
